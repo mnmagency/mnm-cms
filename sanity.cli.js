9744a87a471bdc9562cc4875eb8afee86107066e
mnmagency <noreply@mnmagency.com>
2026-05-14 09:21:01 +0300

    Pin deployment appId in sanity.cli.js

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,13 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'q7p7mofd',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
-  }
+    appId: 'bek1aklc07sgo1vdt57xhviq',
+  },
 })
